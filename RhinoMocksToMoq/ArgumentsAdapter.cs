dbabd755c746009c75b44c3e6d1c6cc214812eb1
Adrian Corduneanu <kordosoft25@gmail.com>
2021-11-02 08:50:34 +0200

    Add project files.

---
+++ RhinoMocksToMoq/ArgumentsAdapter.cs
@@ -0,0 +1,48 @@
+﻿namespace RhinoMocksToMoq
+{
+    using System;
+    using System.Linq;
+    using System.Linq.Expressions;
+    using System.Reflection;
+    using Moq;
+
+    internal sealed class ArgumentsAdapter
+    {
+        private static readonly MethodInfo isAnyMethod = typeof(It).GetMethod(nameof(It.IsAny), BindingFlags.Public | BindingFlags.Static);
+
+        internal static MethodCallExpression IsAny(Type genericArgument)
+        {
+            return Expression.Call(isAnyMethod.MakeGenericMethod(genericArgument));
+        }
+    }
+
+    internal sealed class ArgumentsAdapter<T, TR> where T : class
+    {
+        public Expression<Func<T, TR>> IgnoreArguments(Expression<Func<T, TR>> expression)
+        {
+            var expressionMethod = (MethodCallExpression)expression.Body;
+
+            var newExpression = expressionMethod.Update(
+                expressionMethod.Object,
+                expressionMethod.Arguments.Select(x => ArgumentsAdapter.IsAny(x.Type)).ToList<Expression>()
+            );
+
+            return expression.Update(newExpression, expression.Parameters);
+        }
+    }
+
+    internal class ArgumentsAdapter<T> where T : class
+    {
+        public Expression<Action<T>> IgnoreArguments(Expression<Action<T>> expression)
+        {
+            var expressionMethod = (MethodCallExpression)expression.Body;
+
+            var newExpression = expressionMethod.Update(
+                expressionMethod.Object,
+                expressionMethod.Arguments.Select(x => ArgumentsAdapter.IsAny(x.Type)).ToList<Expression>()
+            );
+
+            return expression.Update(newExpression, expression.Parameters);
+        }
+    }
+}
---
+++ RhinoMocksToMoq/Expect.cs
@@ -0,0 +1,140 @@
+﻿namespace Rhino.Mocks
+{
+    using System;
+    using System.Linq.Expressions;
+    using Moq;
+
+    public interface IExpect<T> where T : class
+    {
+        IRepeat<T> Repeat { get; }
+
+        void Throw(Exception exception);
+
+        IExpect<T> SetupWithMoq();
+        
+        IExpect<T> IgnoreArguments();
+    }
+    
+    public interface IExpect<T, TR> where T : class
+    {
+        IRepeat<T, TR> Repeat { get; }
+
+        IExpect<T, TR> Return(TR result);
+
+        void ReturnInOrder(params TR[] results);
+
+        void Throw(Exception exception);
+        
+        IExpect<T, TR> SetupWithMoq();
+
+        IExpect<T, TR> IgnoreArguments();
+    }
+
+    public class Expect<T, TR> : IExpect<T, TR> where T : class
+    {
+        private readonly MoqAdapter<T, TR> _moqAdapter;
+        
+        private Repeat<T, TR> _repeat;
+
+        private TR _result;
+
+        private bool _isResultAssigned;
+
+        public Expect(Mock<T> mock, Expression<Func<T, TR>> expression)
+        {
+            this._moqAdapter = new MoqAdapter<T, TR>(mock, expression);
+        }
+
+        public IExpect<T, TR> Return(TR result)
+        {
+            if (this._isResultAssigned)
+            {
+                throw new InvalidOperationException("Return should be setup only once");
+            }
+
+            this._isResultAssigned = true;
+            this._result = result;
+
+            return this.SetupWithMoq();
+        }
+
+        public void Throw(Exception exception)
+        {
+            this._moqAdapter.Throws(exception);
+        }
+
+        public IExpect<T, TR> SetupWithMoq()
+        {
+            this._moqAdapter.Setup(this._result, this._repeat);
+
+            return this;
+        }
+
+        public void ReturnInOrder(params TR[] results)
+        {
+            this._moqAdapter.SetupReturnInOrder(results);
+        }
+
+        public IExpect<T, TR> IgnoreArguments()
+        {
+            this._moqAdapter.IgnoreArguments();
+
+            return this;
+        }
+
+        public IRepeat<T, TR> Repeat
+        {
+            get
+            {
+                if (this._repeat != null)
+                {
+                    throw new InvalidOperationException("Repeat should be setup only once");
+                }
+
+                return this._repeat = new Repeat<T, TR>(this);
+            }
+        }
+    }
+
+    public class Expect<T> : IExpect<T> where T : class
+    {
+        private readonly MoqAdapter<T> _moqAdapter;
+
+        private Repeat<T> _repeat;
+
+        public Expect(Mock<T> mock, Expression<Action<T>> expression)
+        {
+            this._moqAdapter = new MoqAdapter<T>(mock, expression);
+        }
+
+        public void Throw(Exception exception)
+        {
+            this._moqAdapter.Throws(exception);
+        }
+
+        public IExpect<T> SetupWithMoq()
+        {
+            this._moqAdapter.Setup(this._repeat);
+            return this;
+        }
+
+        public IExpect<T> IgnoreArguments()
+        {
+            this._moqAdapter.IgnoreArguments();
+
+            return this;
+        }
+
+        public IRepeat<T> Repeat
+        {
+            get
+            {
+                if (this._repeat != null)
+                {
+                    throw new InvalidOperationException("Repeat should setup only once");
+                }
+                return this._repeat = new Repeat<T>(this);
+            }
+        }
+    }
+}
