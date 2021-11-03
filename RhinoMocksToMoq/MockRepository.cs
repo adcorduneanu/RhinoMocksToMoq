fb0a2dfeb758d68a711554507667ca8045b84fdd
kordosoft <kordosoft25@gmail.com>
2021-11-03 08:54:04 +0200

    Remove mockStore

--- RhinoMocksToMoq/MockRepository.cs
+++ RhinoMocksToMoq/MockRepository.cs
@@ -1,20 +1,12 @@
 ﻿namespace Rhino.Mocks
 {
-    using System;
-    using System.Collections.Concurrent;
-    using Moq;
+	using Moq;
 
-    public static class MockRepository
+	public static class MockRepository
     {
-        private static readonly ConcurrentDictionary<object, Mock> MockStore = new ConcurrentDictionary<object, Mock>();
-
         public static Mock<T> Get<T>(T obj) where T : class
         {
-            if (MockStore.TryGetValue(obj, out var mock))
-            {
-                return (Mock<T>) mock;
-            }
-            throw new ArgumentOutOfRangeException(nameof(obj), "Could not find object from mock collection");
+	        return Mock.Get(obj);
         }
 
         public static T GenerateStub<T>() where T : class
@@ -29,22 +21,12 @@
 
         public static T GenerateMock<T>(params object[] args) where T : class
         {
-            var mock = new Mock<T>(args);
-            if (MockStore.TryAdd(mock.Object, mock))
-            {
-                return mock.Object;
-            }
-            throw new Exception("Failed to GenerateMock");
+            return new Mock<T>(args).Object;
         }
 
         public static T GenerateMock<T>(MockBehavior behavior = MockBehavior.Default) where T : class
         {
-            var mock = new Mock<T>(behavior);
-            if (MockStore.TryAdd(mock.Object, mock))
-            {
-                return mock.Object;
-            }
-            throw new Exception("Failed to GenerateMock");
+            return new Mock<T>(behavior).Object;
         }
     }
 }
